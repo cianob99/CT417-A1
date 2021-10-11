1ff11fb21416a73284fcf02331b104cdf56a183e
Cian O'Boyle <cianob99@live.com>
2021-10-11 13:31:09 +0100

    1B initial commit

---
+++ CT417 Assignment 1B/src/main/java/CT417Assignment1B/Main.java
@@ -0,0 +1,17 @@
+
+package CT417Assignment1B;
+
+/**
+ *
+ * @author Cian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
